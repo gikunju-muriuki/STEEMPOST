7e41d4b5545322a2f78a517d5729ffc0b7ab99ad
GIKUNJU MURIUKI <message.egm@gmail.com>
2026-09-17 14:14:01 +0300

    Update post_bot.py

--- post_bot.py
+++ post_bot.py
@@ -1,3 +1,4 @@
+"""
 import os
 import datetime
 from beem import Steem
@@ -245,3 +246,4 @@ try:
 except Exception as e:
     print(f"CRITICAL ERROR: Broadcast routing failed: {e}")
     exit(1)
+    """
