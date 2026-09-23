f7d0b9b322c808ebb2d8e2afa826a6d245904aba
GIKUNJU MURIUKI <message.egm@gmail.com>
2026-09-23 21:07:13 +0300

    Update post_bot.py

--- post_bot.py
+++ post_bot.py
@@ -26,10 +26,11 @@ CUSTOM_TAGS = ["crypto", "xrp", "eth", "bnb", "sol", "btc", "krsuccess"]
 # 20 Hardcoded ultra-low RC asset images mapped explicitly (4 per coin asset category)
 IMAGE_MATRIX = {
     'btc': [
-        "https://steemitimages.com",
-        "https://steemitimages.com",
-        "https://steemitimages.com",
-        "https://steemitimages.com"
+        "https://cdn.steemitimages.com/DQmc1LphNvohBCUsroZ9zjRuL3VzaVJfMVN9oN8hDmX5M9A/1000024867.jpg" ,
+        "https://cdn.steemitimages.com/DQmQjKH5D34ym38roHSoCKh7W79GaV3f6Hh3FNAf7zH6x4K/1000024869.jpg",
+        "https://cdn.steemitimages.com/DQmPNweKd69d1esmMV7DDFFkqs1oPYjEP4TMRqx7TKySk4F/1000024870.jpg",
+        "https://cdn.steemitimages.com/DQmVphp18Aq5jdTu2VsvaQ8SK22FQrsoHHMvCnivwFn2ECz/1000024872.jpg"
+
     ],
     'eth': [
         "https://steemitimages.com",
